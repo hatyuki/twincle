bfdeb328abde0627f7b4d3e600c2b4b1758cce48
eys_hattori <yukihide_hattori@gmx.yamaha.com>
2013-01-07 21:38:30 +0900

    Checking in changes prior to tagging of version 0.02
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 7870977..fa779ee 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,3 +1,7 @@
    +0.03  2013-01-07
    +  - MOD: Update start-up script
    +  - FIX: fix typo
    +
     0.02  2013-01-03
       - ADD: Sending heartbeate message in order to keep WebSocket connection
       - ADD: Implement error handler for twitter OAuth

--- lib/Twincle.pm
+++ lib/Twincle.pm
@@ -2,7 +2,7 @@ package Twincle;
 use strict;
 use warnings;
 use parent qw/ Amon2 /;
-our $VERSION='0.02';
+our $VERSION='0.03';
 use Twincle::Config;
 
 sub load_config { Twincle::Config->current }
